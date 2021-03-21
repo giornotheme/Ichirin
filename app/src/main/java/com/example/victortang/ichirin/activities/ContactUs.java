2acf76d6f0de6acf4577f812fd1f5d06c1ada36d
giornotheme <vtang@outlook.fr>
2021-03-21 23:16:39 +0100

    new things for activities

--- app/src/main/java/com/example/victortang/ichirin/activities/ContactUs.java
+++ app/src/main/java/com/example/victortang/ichirin/activities/ContactUs.java
@@ -2,15 +2,26 @@ package com.example.victortang.ichirin.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.victortang.ichirin.R;
 
 public class ContactUs extends AppCompatActivity {
 
+    private Button mInformation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+
+        mInformation = findViewById(R.id.information);
+
+        mInformation.setOnClickListener(v -> {
+            Intent informationActivity = new Intent(ContactUs.this, Information.class);
+            startActivity(informationActivity);
+        });
     }
 }
--- app/src/main/java/com/example/victortang/ichirin/activities/Information.java
+++ app/src/main/java/com/example/victortang/ichirin/activities/Information.java
@@ -2,15 +2,26 @@ package com.example.victortang.ichirin.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.victortang.ichirin.R;
 
 public class Information extends AppCompatActivity {
 
+    private Button mContactUs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+
+        mContactUs = findViewById(R.id.contactus);
+
+        mContactUs.setOnClickListener(v -> {
+            Intent contactActivity = new Intent(Information.this, ContactUs.class);
+            startActivity(contactActivity);
+        });
     }
 }
